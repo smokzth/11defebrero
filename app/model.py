a2edba1ee59e23cd662e67dfb5187ef20b084e27
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-02-11 11:19:36 -0500

    third commit

--- app/model.py
+++ app/model.py
@@ -6,3 +6,9 @@ class Producto:
 
     def mostrar_info(self):
         return f"{self.nombre} - {self.precio}"
+p1: Producto = Producto("Arroz", 10000)
+p2: Producto = Producto("Crema dental", 5000)
+
+print(p1.mostrar_info())
+print(p2.mostrar_info())
+

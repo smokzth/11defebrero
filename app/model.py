6faa333cba0333c5a8eedd57a03b2f7fe2287a8a
Juan Pablo <francopablo155@gmail.com>
2025-02-13 10:14:29 -0500

    Commit 13 febrero

--- app/model.py
+++ app/model.py
@@ -1,14 +1,45 @@
 class Producto:
 
-    def __init__(self, nombre: str, precio: float):
+    def _init_ (self, nombre: str, precio: float):
         self.nombre: str = nombre
         self.precio: float = precio
 
-    def mostrar_info(self):
+    def mostrar_info(self) -> str:
         return f"{self.nombre} - {self.precio}"
-p1: Producto = Producto("Arroz", 10000)
-p2: Producto = Producto("Crema dental", 5000)
 
-print(p1.mostrar_info())
-print(p2.mostrar_info())
 
+
+class cliente:
+    def _init_(self, nombre: str):
+        self.carrito: str = nombre
+        self.carrito: list[producto] = []
+
+    def agregar_producto(self, producto: Producto):
+        self.carrito.append(producto)
+
+    def mostrar_carrito(self) -> str:
+        carrito_str: str = ""
+        for producto in self.carrito:
+            carrito_str += producto.mostrar_info() + "\n"
+        return carrito_str
+
+    def calcular_total(self) -> float:
+        total: float = 0
+        for producto in self.carrito:
+            total += producto.precio
+
+        return total
+
+class tienda:
+    def _init_(self, nombre: str):
+        self.nombre: str = nombre
+        self.productos:list[Producto] = []
+
+    def agregar_producto(self, producto: Producto):
+        self.productos.append(producto)
+
+    def mostrar_productos(self) -> str:
+        productos_str: str = ""
+        for producto in self.productos:
+            productos_str += producto.mostrar_info() + "\n"
+        return productos_str
